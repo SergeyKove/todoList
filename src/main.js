c2022de4a6de990a8860427d573eec6bd838075f
Sergey Koveshnikov <sergey.kove@mail.ru>
2025-02-25 11:24:30 +0300

    new

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Quasar } from 'quasar'
+import quasarUserOptions from './quasar-user-options'
 
-const app = createApp(App)
+const app = createApp(App).use(Quasar, quasarUserOptions)
 
 app.use(createPinia())
 
